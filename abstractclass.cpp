dc478f08851c619c5aaeec4b1961cf559458d711
Priyansh67 <priyanshnamdev@outlook.com>
2021-10-14 20:27:42 +0530

    Abstract class

---
+++ abstractclass.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+/*program for abstract class
+
+*/
+class car       // asbtract class
+{
+public:
+    virtual void start() = 0;
+};
+class innova : public car
+{
+public:
+    void start()
+    {
+        cout << "innova started" << endl;
+    }
+};
+class swift : public car
+{
+public:
+    void start()
+    {
+        cout << "swift started" << endl;
+    }
+};
+int main()
+{
+    //car c;
+    car *ptr = new innova();
+    ptr->start();
+    ptr = new swift();
+    ptr->start();
+    return 0;
+}
