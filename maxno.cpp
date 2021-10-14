9bf0503d89782411bcb5e01d6b5e24a6705b6895
Priyansh67 <priyanshnamdev@outlook.com>
2021-10-14 20:23:48 +0530

    Maximum of 2 no.

---
+++ maxno.cpp
@@ -0,0 +1,22 @@
+# include<iostream>
+using namespace std;
+    
+//program for finding maximum of 2 nos using conditional statements
+
+int main() 			
+ {
+	
+	int x,y;
+
+	cout<<"enter 2 nos\n";
+	cin>>x>>y;
+	if(x>y)
+	{
+		cout<<"maximum is "<<x<<endl;
+	}
+	else
+	{
+		cout<<"maximum is "<<y<<endl;
+	}
+	return 0;
+ }
