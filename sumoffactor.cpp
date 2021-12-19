82a4f3df4f3e44a11996db6f720c2d6a96d2115f
Priyansh67 <priyanshnamdev@outlook.com>
2021-12-19 13:14:29 +0530

    Sum of factors

---
+++ sumoffactor.cpp
@@ -0,0 +1,18 @@
+# include<iostream>
+using namespace std;
+    
+int main()
+{
+	int i,n,sum=0;
+	cout<<"Enter the number\n";
+    cin>>n;
+	for(i=1;i<=n;i++)
+	{
+	     if(n%i==0)
+	     {
+		     sum=sum+i;
+	     }
+	}  
+	cout<<"Sum of factors is "<<sum<<endl;     
+	return 0;
+}
