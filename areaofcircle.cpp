4665f72c9bf17f2b7e1025b44f0fafcb7d9ae20e
Priyansh67 <priyanshnamdev@outlook.com>
2021-12-20 20:08:44 +0530

    Circle area

---
+++ areaofcircle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    float r,area;
+    cout<<"Enter the Radius\n";
+    cin>>r;
+    area=3.14*r*r;
+    cout<<"Area is "<<area;
+    return 0;
+}
